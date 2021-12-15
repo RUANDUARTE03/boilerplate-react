4e0c2bf230a3a5d14466d8a9809deea61952a5ff
ruanduarte03 <ruanduarte0300@gmail.com>
2021-12-14 23:30:45 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <h1>Boilerplate</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<h1>PATH1</h1>} />
+        <Route path={'/test'} element={<h1>PATH2</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
